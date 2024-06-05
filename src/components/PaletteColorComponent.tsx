33cafcec408d0e07a5f04a6c9ca8a6d8fd56a208
Gabriel <gabrielsantosterra@gmail.com>
2024-06-05 17:31:03 -0300

    Inicialização de componente - Criação do componente PaletteColorComponent

---
+++ src/components/PaletteColorComponent.tsx
@@ -0,0 +1,27 @@
+export const palleteColors = {
+  // Button colors
+  bnone: "",
+  bprimary: "",
+  bsecondary: "",
+
+  //Label colors
+  lnone: "",
+  lprimary: "",
+  lsecondary: "",
+
+  //Link colors
+  unone: "",
+  uprimary: "",
+  usecondary: "",
+
+  //Checkbox colors
+  cnone: "",
+  cpripary: "",
+  csecondary: "",
+
+  //Input colors
+  inone: "",
+  iprimary: "",
+  isecondary: "",
+
+};
